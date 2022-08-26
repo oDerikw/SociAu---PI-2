4f52e8c6bcc32a03dd0fb5e0d7b27a2596004ced
Daniel Volski <danfvdaum3@hotmail.com>
2022-08-25 22:17:56 -0300

    Added name and id parameter to checkboxes

--- resources/views/addPerguntas.blade.php
+++ resources/views/addPerguntas.blade.php
@@ -7,49 +7,48 @@
     <button type="submit">Logout</button>
 </form>
 
-<form name="formAdd" id="formAdd" method="post" action="">
+<form name="formAdd" id="formAdd" method="post" action="{{ route('questionRegister') }}">
     @csrf
     <h2>CADASTRAR QUESTÃO</h2>
-    <p type="Categoria"><input type="text" name="cpf" id="cpf" ></p>
+    <p type="Categoria"><input type="text" name="category" id="category" ></p>
         
-    <p type="Pergunta"><input type="text" name="cpf" id="cpf" ></p>
+    <p type="Pergunta"><input type="text" name="question" id="question" ></p>
 
-    <p type="Explicação Errada"><input type="text" name="cpf" id="cpf" ></p>
+    <p type="Explicação Errada"><input type="text" name="explanation" id="explanation" ></p>
 
-    <p type="Imagem"><input type="text" name="cpf" id="cpf" ></p>
-
-    <p type="Alternativa:"><input type="text" name="title" id="title"></p>
+    <p type="Imagem"><input type="text" name="image" id="image" ></p>
+    <p type="Alternativa:"><input type="text" name="alt1" id="alt1"></p>
     <br>
     <label class="corretude">Correta<br>
-        <input type="checkbox" name=corretude checked="checked">
-        <span class="checkmark"></span>
-    </label>
-        
-    <p type="Alternativa"><input type="text" name="cpf" id="cpf" ></p>
-    <br>
-    <label class="corretude">Correta<br>
-        <input type="checkbox" name=corretude checked="checked">
-        <span class="checkmark"></span>
-    </label>
-
-    <p type="Alternativa:"><input type="text" name="title" id="title"></p>
-    <br>
-    <label class="corretude">Correta<br>
-        <input type="checkbox" name=corretude checked="checked">
+        <input type="checkbox" name="isCorrect1" checked="checked" value="1">
         <span class="checkmark"></span>
     </label>
         
-    <p type="Alternativa:"><input type="text" name="title" id="title"></p>
+    <p type="Alternativa"><input type="text" name="alt2" id="alt2"></p>
     <br>
     <label class="corretude">Correta<br>
-        <input type="checkbox" name=corretude checked="checked">
+        <input type="checkbox" name="isCorrect2" checked="checked" value="1">
         <span class="checkmark"></span>
     </label>
 
-    <p type="Alternativa:"><input type="text" name="title" id="title"></p>
+    <p type="Alternativa:"><input type="text" name="alt3" id="alt3"></p>
     <br>
     <label class="corretude">Correta<br>
-        <input type="checkbox" name=corretude checked="checked">
+        <input type="checkbox" name="isCorrect" checked="checked" value="1">
+        <span class="checkmark"></span>
+    </label>
+        
+    <p type="Alternativa:"><input type="text" name="alt4" id="alt4"></p>
+    <br>
+    <label class="corretude">Correta<br>
+        <input type="checkbox" name="isCorrect" checked="checked" value="1">
+        <span class="checkmark"></span>
+    </label>
+
+    <p type="Alternativa:"><input type="text" name="alt5" id="alt5"></p>
+    <br>
+    <label class="corretude">Correta<br>
+        <input type="checkbox" name="isCorrect" checked="checked" value="1">
         <span class="checkmark"></span>
     </label>
     <br>
