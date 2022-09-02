4f0a79b98b7c9a4ae3f3fa74a17ad08eac4cf997
Daniel Volski <danfvdaum3@hotmail.com>
2022-09-01 22:41:48 -0300

    The class was refactored to eliminate code redundancy

--- app/Http/Controllers/RetrieveQuestionController.php
+++ app/Http/Controllers/RetrieveQuestionController.php
@@ -9,122 +9,78 @@ use App\Models\Question;
 class RetrieveQuestionController extends Controller
 {
     private $accepted_categories = ['empatia', 'cotidiano', 'sentimentos', 'higiene'];
-    private $accepted_difficulties = ['facil', 'medio', 'dififcil'];
+    private $accepted_difficulties = ['facil', 'medio', 'dificil'];
 
     public function retrieve($category, $difficulty)
     {
-        if ($this->validateRequestArguments($category, $difficulty)) { return redirect('jogar'); }
+        if (!$this->validateRequestArguments($category, $difficulty)) { return redirect('jogar'); }
 
         // Seleciona uma questão aleatória com base na categoria selecionada
         $query_question = Question::where('category', $category)->inRandomOrder()->get(['id', 'question', 'explanation', 'image']);
+        
 
         switch ($difficulty)
         {
             case 'facil':
-                // Seleciona alternativas incorretas aleatórias com base no id da questão
-                $query_alt_incorrect = Alternative::where([
-                    ['question_id', '=', $query_question[0]->id],
-                    ['isCorrect', '=', 0]
-                ])->limit(1)->inRandomOrder()->get(['id', 'alternative']);
-
-                // Seleciona uma alternativa correta aleatória com base no id da questão
-                $query_alt_correct = Alternative::where([
-                    ['question_id', '=', $query_question[0]->id],
-                    ['isCorrect', '=', 1]
-                ])->limit(1)->inRandomOrder()->get(['id', 'alternative']);
-
-                $alternatives = [
-                    $query_alt_incorrect[0],
-                    $query_alt_correct[0],
-                ];
+                $alternatives = $this->queryAlternatives($query_question[0]->id, 1, 1);
+                break;
                 
-                // Embaralha as alternativas
-                shuffle($alternatives);
-
-                $data = [
-                    'question' => $query_question[0]->question,
-                    'explanation' => $query_question[0]->explanation,
-                    'image' => $query_question[0]->image,
-                    'alternatives' => $alternatives,
-                ];
-
-                $query_question = $query_alt_incorrect = $query_alt_incorrect = NULL;
-
-                return view('quiz/'.$category , $data);
             case 'medio':
-                // Seleciona alternativas incorretas aleatórias com base no id da questão
-                $query_alt_incorrect = Alternative::where([
-                    ['question_id', '=', $query_question[0]->id],
-                    ['isCorrect', '=', 0]
-                ])->limit(2)->inRandomOrder()->get(['id', 'alternative']);
-
-                // Seleciona uma alternativa correta aleatória com base no id da questão
-                $query_alt_correct = Alternative::where([
-                    ['question_id', '=', $query_question[0]->id],
-                    ['isCorrect', '=', 1]
-                ])->limit(2)->inRandomOrder()->get(['id', 'alternative']);
-
-                $alternatives = [
-                    $query_alt_incorrect[0],
-                    $query_alt_incorrect[1],
-                    $query_alt_correct[0],
-                    $query_alt_correct[1],
-                ];
+                $alternatives = $this->queryAlternatives($query_question[0]->id, 2, 2);
+                break;
                 
-                // Embaralha as alternativas
-                shuffle($alternatives);
-                
-                $data = [
-                    'question' => $query_question[0]->question,
-                    'explanation' => $query_question[0]->explanation,
-                    'image' => $query_question[0]->image,
-                    'alternatives' => $alternatives,
-                ];
-
-                $query_question = $query_alt_incorrect = $query_alt_incorrect = NULL;
-
-                return view('quiz/'.$category, $data);
             case 'dificil':
-                // Seleciona alternativas incorretas aleatórias com base no id da questão
-                $query_alt_incorrect = Alternative::where([
-                    ['question_id', '=', $query_question[0]->id],
-                    ['isCorrect', '=', 0]
-                ])->limit(3)->inRandomOrder()->get(['id', 'alternative']);
-
-                // Seleciona uma alternativa correta aleatória com base no id da questão
-                $query_alt_correct = Alternative::where([
-                    ['question_id', '=', $query_question[0]->id],
-                    ['isCorrect', '=', 1]
-                ])->limit(1)->inRandomOrder()->get(['id', 'alternative']);
-
-                $alternatives = [
-                    $query_alt_incorrect[0],
-                    $query_alt_incorrect[1],
-                    $query_alt_incorrect[2],
-                    $query_alt_correct[0],
-                ];
-                
-                // Embaralha as alternativas
-                shuffle($alternatives);
-                
-                $data = [
-                    'question' => $query_question[0]->question,
-                    'explanation' => $query_question[0]->explanation,
-                    'image' => $query_question[0]->image,
-                    'alternatives' => $alternatives,
-                ];
-
-                $query_question = $query_alt_incorrect = $query_alt_incorrect = NULL;
-                return view('quiz/'.$category, $data);
+                $alternatives = $this->queryAlternatives($query_question[0]->id, 3, 1);
+                break;
         }
+
+        // Embaralha as alternativas
+        shuffle($alternatives);
+                
+        $data = [
+            'question' => $query_question[0]->question,
+            'explanation' => $query_question[0]->explanation,
+            'image' => $query_question[0]->image,
+            'alternatives' => $alternatives,
+        ];
+        $query_question = NULL;
+
+        return view('quiz/'.$category, $data);
     }
 
     private function validateRequestArguments($category, $difficulty)
     {
         if (in_array($category, $this->accepted_categories, true) and in_array($difficulty, $this->accepted_difficulties, true))
         {
-            return false;
+            return true;
         }
-        return true;
+        return false;
+    }
+
+    private function queryAlternatives($question_id, $qt_incorrect, $qt_correct)
+    {
+        $alternatives = [];
+        
+        // Seleciona alternativas incorretas aleatórias com base no id da questão
+        $query_incorrect_alt = Alternative::where([
+            ['question_id', '=', $question_id],
+            ['isCorrect', '=', 0]
+        ])->limit($qt_incorrect)->inRandomOrder()->get(['id', 'alternative']);
+
+        // Seleciona uma alternativa correta aleatória com base no id da questão
+        $query_correct_alt = Alternative::where([
+            ['question_id', '=', $question_id],
+            ['isCorrect', '=', 1]
+        ])->limit($qt_correct)->inRandomOrder()->get(['id', 'alternative']);
+
+        foreach ($query_incorrect_alt as $incorrect_alt)
+            array_push($alternatives, $incorrect_alt);
+
+        foreach ($query_correct_alt as $correct_alt)
+            array_push($alternatives, $correct_alt);
+
+        $query_incorrect_alt = $query_correct_alt = NULL;
+
+        return $alternatives;
     }
 }
